c483611d393969daf3ba0e97bce925271fcf6bb0
Yoann <41453809+YoannMourot@users.noreply.github.com>
2019-05-21 15:28:21 +0200

    bdd a jour

--- mabel.sql
+++ mabel.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  lun. 20 mai 2019 à 13:52
+-- Généré le :  mar. 21 mai 2019 à 13:27
 -- Version du serveur :  5.7.24
 -- Version de PHP :  7.2.14
 
@@ -25,6 +25,28 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `categories`
+--
+
+DROP TABLE IF EXISTS `categories`;
+CREATE TABLE IF NOT EXISTS `categories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `shopid` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `categories`
+--
+
+INSERT INTO `categories` (`id`, `shopid`, `name`) VALUES
+(14, 25, 'nouvelle catégorie'),
+(13, 25, 'categorie');
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `items`
 --
 
@@ -32,27 +54,27 @@ DROP TABLE IF EXISTS `items`;
 CREATE TABLE IF NOT EXISTS `items` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `shopid` int(11) NOT NULL,
-  `category` varchar(255) NOT NULL,
+  `category` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `description` text,
   `price` int(11) DEFAULT NULL,
-  `picture` varchar(255) DEFAULT NULL,
+  `picture1` varchar(255) DEFAULT NULL,
+  `picture2` varchar(255) DEFAULT NULL,
+  `picture3` varchar(255) DEFAULT NULL,
   `quantity` smallint(6) NOT NULL DEFAULT '1',
   `moreinfo` text,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=37 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `items`
 --
 
-INSERT INTO `items` (`id`, `shopid`, `category`, `name`, `description`, `price`, `picture`, `quantity`, `moreinfo`) VALUES
-(2, 15, 'test', 'coucou', NULL, NULL, NULL, 1, NULL),
-(3, 15, 'test', 'la shoes', NULL, NULL, NULL, 1, NULL),
-(18, 15, 'test', 'rere', NULL, NULL, NULL, 1, NULL),
-(7, 15, 'chaussurerouge', 'nikesamr', NULL, NULL, NULL, 1, NULL),
-(16, 15, 'test', 'test', NULL, NULL, NULL, 1, NULL),
-(17, 15, 'test', 'retest', NULL, NULL, NULL, 1, NULL);
+INSERT INTO `items` (`id`, `shopid`, `category`, `name`, `description`, `price`, `picture1`, `picture2`, `picture3`, `quantity`, `moreinfo`) VALUES
+(33, 25, 13, 'unobjet', NULL, NULL, 'd2wDYjcegFMourot22itemimg33number1shopid25.png', '', '', 1, NULL),
+(34, 25, 13, 'deuxièmeobjet', NULL, NULL, '8fyLRAzAq4Mourot22itemimg34number1shopid25.jpg', NULL, NULL, 1, NULL),
+(35, 25, 13, 'coucou', NULL, NULL, NULL, NULL, NULL, 1, NULL),
+(36, 25, 14, 'papier peint', NULL, NULL, 'Xn6tHlGDCnMourot22itemimg36number1shopid25.jpg', NULL, NULL, 1, NULL);
 
 -- --------------------------------------------------------
 
@@ -75,18 +97,15 @@ CREATE TABLE IF NOT EXISTS `shops` (
   `status` varchar(255) NOT NULL DEFAULT 'offline',
   `creation_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`shopid`)
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `shops`
 --
 
 INSERT INTO `shops` (`shopid`, `userid`, `name`, `headerpresent`, `headercolor`, `shoplogo`, `navbarpresent`, `aboutuspresent`, `about_us_text`, `about_us_img`, `status`, `creation_date`) VALUES
-(14, 25, 'testlol2', 1, '#283F6D', 'logoshopsample.png', 1, 1, NULL, NULL, 'offline', '2019-05-18 19:30:06'),
-(15, 22, 'fgvk', 1, '#283F6D', 'logoshopsample.png', 1, 1, NULL, NULL, 'offline', '2019-05-18 20:05:59'),
-(16, 22, 'rr\"r', 1, '#283F6D', 'logoshopsample.png', 1, 1, NULL, NULL, 'offline', '2019-05-19 16:39:56'),
-(18, 22, 'test', 1, '#283F6D', 'logoshopsample.png', 1, 1, NULL, NULL, 'offline', '2019-05-19 16:48:08'),
-(19, 22, 'f\"r\"\'f\'\"', 1, '#283F6D', 'logoshopsample.png', 1, 1, NULL, NULL, 'offline', '2019-05-19 16:48:37');
+(25, 22, 'phonshop', 1, '#283F6D', 'NwRuHr4NmVMourot22shoplogo25.png', 1, 1, NULL, NULL, 'offline', '2019-05-21 12:52:45'),
+(26, 22, 'caca', 1, '#283F6D', 'Gjg4UZgoDIMourot22shoplogo26.jpg', 1, 1, NULL, NULL, 'offline', '2019-05-21 13:25:04');
 
 -- --------------------------------------------------------
 
@@ -112,7 +131,7 @@ CREATE TABLE IF NOT EXISTS `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `firstname`, `profilepic`, `password`, `mail`, `inscription_date`, `token`) VALUES
-(22, 'Mourot', 'Yoann', 'Mourot7s9iWOcTvK.jpg', '$2y$10$dUNEv5zU32h02vT6twjV9eznDaB7X/mwMeOHodqTQjrKb7WcG0Qx2', 'yoann.mourot@outlook.fr', '2019-05-17 17:47:05', NULL),
+(22, 'Mourot', 'Yoann', 'MourotiMZM8rmIEs.jpg', '$2y$10$nfG.W3gk/WF4tt7JN5TGfuT4WZsw.k3inLy8.MZzbpbGcHRwxrwXG', 'yoann.mourot@outlook.fr', '2019-05-17 17:47:05', ''),
 (23, 'gggggg', 'ggggg', NULL, '$2y$10$FBsqxv8mXv8V9hplR1Km9eZvLbgNVOw2xVt1U8ViDlw1HCasjEMHW', 'test@free.fr', '2019-05-18 18:09:22', NULL),
 (24, 'ddddddddd', 'dddddd', NULL, '$2y$10$cNAUm04U6IXkTttPXA92r.fEe68QSsXgzTVS9IYfFaqHIAhVZjHuO', 'yoann.mourot1@gmai.com', '2019-05-18 18:20:26', NULL),
 (25, 'ddddddddd', 'dddddd', NULL, '$2y$10$.qG9WzOe9iTdyhalZGSP8.nQmREu2svIEeqLLBkMdeUQ1ckHOypjS', 'yoann.mourot3@gmail.com', '2019-05-18 19:28:57', NULL);
